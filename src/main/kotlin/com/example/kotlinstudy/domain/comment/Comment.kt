842476543bb8287d9c5166d1761f19a2bcf51ede
LeeChungHyeon <lee855916234@gmail.com>
2025-01-15 14:42:28 +0900

    add to comment entity

--- src/main/kotlin/com/example/kotlinstudy/domain/comment/Comment.kt
+++ src/main/kotlin/com/example/kotlinstudy/domain/comment/Comment.kt
@@ -1,8 +1,18 @@
 package com.example.kotlinstudy.domain.comment
 
 import com.example.kotlinstudy.domain.AuditingEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 
+@Entity
+@Table(name = "Comment")
 class Comment(
-
+    content: String,
 ) : AuditingEntity() {
+
+    @Column(name = "content", nullable = false)
+    var content: String = content
+        protected set
+
 }
